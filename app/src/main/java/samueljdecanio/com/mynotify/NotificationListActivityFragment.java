d966bfe9a5ab7ce5217b2c5d3a6631121c62d11c
Samuel Decanio <sjdecanio@gmail.com>
2018-11-14 22:17:28 -0800

    Initial Commit v2

---
+++ app/src/main/java/samueljdecanio/com/mynotify/NotificationListActivityFragment.java
@@ -0,0 +1,22 @@
+package samueljdecanio.com.mynotify;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NotificationListActivityFragment extends Fragment {
+
+    public NotificationListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notification_list, container, false);
+    }
+}
